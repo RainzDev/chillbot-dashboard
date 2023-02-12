73f0205431b74c8415ea2e743c06f0befe6727c0
rainzDev <62964686+RainzDev@users.noreply.github.com>
2023-02-13 01:43:20 +0300

    Delete constants.py

--- constants.py
+++
@@ -1,1 +0,0 @@
-OAUTH_URI = "https://discord.com/api/oauth2/authorize?client_id=848384657774084107&redirect_uri=https%3A%2F%2Fchillbot.ml%2Foauth%2Fcallback&response_type=code&scope=identify%20guilds"
